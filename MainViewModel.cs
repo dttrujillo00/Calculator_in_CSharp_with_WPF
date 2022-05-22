62f8522b4d34adc931bfac6fefb29de47795534d
dttrujillo00 <dttrujillo00@gmail.com>
2022-05-22 16:54:28 -0400

    Calculadora funcional con View-ViewModel

---
+++ MainViewModel.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace Calculator
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private ICommand _clickCommandSum;
+        private ICommand _clickCommandSub;
+        private ICommand _clickCommandMultiply;
+        private ICommand _clickCommandDivide;
+        public string PropertyForResult { get; set; } = string.Empty;
+        public string PropertyForInput1 { get; set; } = string.Empty;
+        public string PropertyForInput2 { get; set; } = string.Empty;
+
+        public ICommand SumCommand
+        {
+            get
+            {
+                return _clickCommandSum ?? (_clickCommandSum = new CommandHandler(() => SumAction(), () => CanExecute));
+            }
+        }
+        public ICommand SubCommand
+        {
+            get
+            {
+                return _clickCommandSub ?? (_clickCommandSub = new CommandHandler(() => SubAction(), () => CanExecute));
+            }
+        }
+
+        public ICommand MultiplyCommand
+        {
+            get
+            {
+                return _clickCommandMultiply ?? (_clickCommandMultiply = new CommandHandler(() => MultiplyAction(), () => CanExecute));
+            }
+        }
+
+        public ICommand DivideCommand
+        {
+            get
+            {
+                return _clickCommandDivide ?? (_clickCommandDivide = new CommandHandler(() => DivideAction(), () => CanExecuteDivide));
+            }
+        }
+
+
+        public void SumAction()
+        {
+            PropertyForResult = (Double.Parse(PropertyForInput1) + Double.Parse(PropertyForInput2)).ToString();
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs("PropertyForResult"));
+        }
+
+        public void SubAction()
+        {
+            PropertyForResult = (Double.Parse(PropertyForInput1) - Double.Parse(PropertyForInput2)).ToString();
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs("PropertyForResult"));
+        }
+
+        public void MultiplyAction()
+        {
+            PropertyForResult = (Double.Parse(PropertyForInput1) * Double.Parse(PropertyForInput2)).ToString();
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs("PropertyForResult"));
+        }
+
+        public void DivideAction()
+        {
+            PropertyForResult = (Double.Parse(PropertyForInput1) / Double.Parse(PropertyForInput2)).ToString();
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs("PropertyForResult"));
+        }
+
+        public bool CanExecute
+        {
+            get
+            {
+                if(PropertyForInput1 != "" && PropertyForInput2 != "")
+                    return true;
+                return false;
+            }
+        }
+
+        public bool CanExecuteDivide
+        {
+            get
+            {
+                if(PropertyForInput1 != "" && PropertyForInput2 != "" && Double.Parse(PropertyForInput2) != 0.0)
+                    return true;
+
+                return false;
+            }
+        }
+    }
+
+    /*********************************************
+     *  IMPLEMENTACION DE LA INTERFAZ ICOMMAND   *
+     *********************************************/
+    public class CommandHandler : ICommand
+    {
+        private Action _action;
+        private Func<bool> _canExecute;
+
+        public CommandHandler(Action action, Func<bool> canExecute)
+        {
+            _action = action;
+            _canExecute = canExecute;
+
+        }
+
+        event EventHandler? ICommand.CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        bool ICommand.CanExecute(object? parameter)
+        {
+            return _canExecute.Invoke();
+        }
+
+        void ICommand.Execute(object? parameter)
+        {
+            _action();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Windows;
+
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            DataContext = new MainViewModel();
+            InitializeComponent();
+           
+        }
+
+        /*private void Convertir(object sender, RoutedEventArgs e)
+        {
+            int decimal_number = (decimal_input.Text == "") ? 0 : Convert.ToInt32(decimal_input.Text);
+            int resto;
+            string binary = string.Empty;
+
+            while (decimal_number > 0)
+            {
+                resto = decimal_number % 2;
+                decimal_number /= 2;
+                binary = resto.ToString() + binary;
+            }
+
+            binary_input.Text = binary;
+        }
+
+        private void toBinary(object sender, KeyEventArgs e)
+        {
+           
+            int decimal_number = Convert.ToInt32(decimal_input.Text);
+            int reminder;
+            string binary = string.Empty;
+
+            while(decimal_number > 0)
+            {
+                reminder = decimal_number % 2;
+                decimal_number /= 2;
+                binary = reminder.ToString() + binary;
+            }
+
+            binary_input.Text = binary;
+        }
+
+        private void toDecimal(object sender, KeyEventArgs e)
+        {
+            string binary = binary_input.Text;
+            int decimal_number = 0;
+            int exponente = 0;
+
+            for(int i = binary.Length - 1; i >= 0; i--)
+            {
+                decimal_number += (Convert.ToInt32(binary[i]) * (int)Math.Pow(2, exponente));
+                exponente++;
+            }
+
+            decimal_input.Text = Convert.ToString(decimal_number);
+        }*/
+
+       
+    }
+}
